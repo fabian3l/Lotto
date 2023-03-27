1a672f74f4e1af3e49b553870b66db12f633b96d
fabian3l <94535567+fabian3l@users.noreply.github.com>
2023-03-27 10:23:23 +0200

    Created modules, tests and facades

---
+++ src/main/java/com/lotto/domain/numberreciver/NumberReciverFacade.java
@@ -0,0 +1,4 @@
+package com.lotto.domain.numberreciver;
+
+public class NumberReciverFacade {
+}
---
+++ src/test/java/com/lotto/domain/numberreciver/NumberReciverFacadeTest.java
@@ -0,0 +1,7 @@
+package com.lotto.domain.numberreciver;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NumberReciverFacadeTest {
+
+}
