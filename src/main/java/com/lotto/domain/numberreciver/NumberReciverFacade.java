7987c1830fa3c665cd45dff79aba9478d5a08fd4
fabian3l <94535567+fabian3l@users.noreply.github.com>
2023-03-27 15:06:48 +0200

    Added NumberReciverFacade and NumberReciverFacadeTest

--- src/main/java/com/lotto/domain/numberreciver/NumberReciverFacade.java
+++ src/main/java/com/lotto/domain/numberreciver/NumberReciverFacade.java
@@ -1,4 +1,33 @@
 package com.lotto.domain.numberreciver;
 
+import com.lotto.domain.numberreciver.dto.InputNumbersResultDto;
+import lombok.AllArgsConstructor;
+
+import java.util.List;
+import java.util.Set;
+
+import static com.lotto.domain.numberreciver.dto.InputNumbersResultDto.*;
+
+//klient podaje 6 liczb
+//liczby musza byc w zakresie 1-99
+//liczby nie moga sie powtarzac
+//klient dostaje informacje o dacie losowania
+//klient dostaje informacje o swoim unikalnym identyfikatorze losowania
+
+@AllArgsConstructor
 public class NumberReciverFacade {
+    private final NumberValidator validator;
+    public InputNumbersResultDto inputNumbers(Set<Integer> numbersFromUser) {
+
+        Boolean areAllNumbersInRange = validator.areAllNumbersInRange(numbersFromUser);
+        if (areAllNumbersInRange) {
+            return builder()
+                    .message("success")
+                    .build();
+        }
+        return builder()
+                .message("failed")
+                .build();
+    }
+
 }
--- src/test/java/com/lotto/domain/numberreciver/NumberReciverFacadeTest.java
+++ src/test/java/com/lotto/domain/numberreciver/NumberReciverFacadeTest.java
@@ -1,7 +1,57 @@
 package com.lotto.domain.numberreciver;
 
+import com.lotto.domain.numberreciver.dto.InputNumbersResultDto;
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
+import static java.util.Set.of;
+import static org.assertj.core.api.Assertions.assertThat;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class NumberReciverFacadeTest {
+
+    NumberReciverFacade numberReciverFacade = new NumberReciverFacade(
+            new NumberValidator()
+    );
+
+    @Test
+    public void should_return_success_when_user_gave_six_numbers() {
+        //given
+        Set<Integer> numbersFromUser = Set.of(1,2,3,4,5,6);
+        //when
+        InputNumbersResultDto result = numberReciverFacade.inputNumbers(numbersFromUser);
+        //then
+        assertThat(result.message()).isEqualTo("success");
+    }
+    @Test
+    public void should_return_failed_when_user_gave_less_than_six_numbers(){
+        //given
+        Set<Integer> numbersFromUser = Set.of(1,2,3,4,5);
+        //when
+        InputNumbersResultDto result = numberReciverFacade.inputNumbers(numbersFromUser);
+        //then
+        assertThat(result.message()).isEqualTo("failed");
+    }
+    @Test
+    public void should_return_failed_when_user_gave_more_than_six_numbers(){
+        //given
+        Set<Integer> numbersFromUser = Set.of(1,2,3,4,5,6, 7);
+        //when
+        InputNumbersResultDto result = numberReciverFacade.inputNumbers(numbersFromUser);
+        //then
+        assertThat(result.message()).isEqualTo("failed");
+    }
+    @Test
+    public void should_return_failed_when_user_gave_at_least_one_number_out_of_range(){
+        //given
+        Set<Integer> numbersFromUser = Set.of(1,2000,3,4,5,6);
+        //when
+        InputNumbersResultDto result = numberReciverFacade.inputNumbers(numbersFromUser);
+        //then
+        assertThat(result.message()).isEqualTo("failed");
+
+    }
 
 }
